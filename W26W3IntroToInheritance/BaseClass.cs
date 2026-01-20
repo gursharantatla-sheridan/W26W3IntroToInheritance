b55902a0fc5f7ce88bda11c39af731c5b50e4e38
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-20 17:29:27 -0500

    access mods

---
+++ W26W3IntroToInheritance/BaseClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace W26W3IntroToInheritance
+{
+    public class BaseClass
+    {
+        // fields
+        private int myPrivateVar;
+        protected int myProtectedVar;
+        public int myPublicVar;
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public void MyMethod()
+        {
+            //myPrivateVar = 1;
+            myProtectedVar = 2;
+            myPublicVar = 3;
+        }
+    }
+}
