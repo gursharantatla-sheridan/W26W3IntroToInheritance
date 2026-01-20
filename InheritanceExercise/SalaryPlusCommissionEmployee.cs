604bf72a1ea56664a1de4caebb1092b083cadad2
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-20 18:26:30 -0500

    sal + comm emp

---
+++ InheritanceExercise/SalaryPlusCommissionEmployee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceExercise
+{
+    public class SalaryPlusCommissionEmployee : CommissionEmployee
+    {
+		private double _salary;
+
+		public double Salary
+		{
+			get { return _salary; }
+			set { _salary = value; }
+		}
+
+
+		public SalaryPlusCommissionEmployee(int id, string name, double grossSales, double commissionRate, double salary) : base(id, name, grossSales, commissionRate)
+		{
+			Salary = salary;
+		}
+
+
+		public new double Earnings()
+		{
+			return Salary + base.Earnings();
+		}
+
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nSalary = {Salary:C}";
+        }
+	}
+}
